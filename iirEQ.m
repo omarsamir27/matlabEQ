a21d89661bd6461c6128b705d87e6ff188a002d6
Omar Mohamed Samir <omarmohameddrive@gmail.com>
2021-06-14 22:34:41 +0200

    Update iirEQ.m
    cleaning

--- iirEQ.m
+++ iirEQ.m
@@ -1,15 +1,8 @@
 classdef iirEQ
     properties
         %%Filter Coeffecients and gain[b,a]%%
-        from0to170 
-        from170to310 
-        from310to600 
-        from600to1K 
-        from1Kto3K 
-        from3Kto6K 
-        from6Kto12K 
-        from12Kto14K 
-        from14kto16K 
+        coeffecients = {}
+        bands = {170,[170 310],[310 600],[600 1000],[1000 3000],[3000 6000],[6000 12000],[12000 14000],[14000 16000]}
         %%Gains Array,same ordering as filter coeffecients%%
         gains
         %%Order Array for filters%%
@@ -21,39 +14,16 @@ classdef iirEQ
         function obj = iirEQ(gains,audio,fs)
             obj.gains = gains;
             fs = fs/2;
-            [b,a] = butter(obj.orders(1),170/fs);
-            obj.from0to170 = {b,a};
-            [b,a] = butter(obj.orders(2),[170 310]/fs);
-            obj.from170to310 = {b,a};
-            [b,a] = butter(obj.orders(3),[310 600]/fs);
-            obj.from310to600 = {b,a};
-            [b,a]= butter(obj.orders(4),[600 1000]/fs);
-            obj.from600to1K = {b,a};
-            [b,a] = butter(obj.orders(5),[1000 3000]/fs);
-            obj.from1Kto3K = {b,a};
-            [b,a] = butter(obj.orders(6),[3000 6000]/fs);
-            obj.from3Kto6K = {b,a};
-            [b,a] = butter(obj.orders(7),[6000 12000]/fs);
-            obj.from6Kto12K = {b,a};
-            [b,a] = butter(obj.orders(8),[12000 14000]/fs);
-            obj.from12Kto14K = {b,a};
-            [b,a] = butter(obj.orders(9),[14000 16000]/fs);
-            obj.from14kto16K = {b,a};
+            for i = 1:9
+                [b,a] = butter(obj.orders(i),obj.bands{i} / fs);
+                obj.coeffecients{i} = {b,a};
+            end
             obj.EqlzdAudio = obj.eqaudio(audio);
         end
         function eqlzd = eqaudio(obj,audio)
             eqlzd = zeros(size(audio,1),size(audio,2));
-            coeffecients = {obj.from0to170;...
-                    obj.from170to310;...
-                    obj.from310to600;...
-                    obj.from600to1K;...
-                    obj.from1Kto3K;...
-                    obj.from3Kto6K;...
-                    obj.from6Kto12K;...
-                    obj.from12Kto14K;...
-                    obj.from14kto16K};
             for i = 1:9
-                currentcoeff = coeffecients{i};
+                currentcoeff = obj.coeffecients{i};
                 eqlzd = filter(obj.gains(i)*currentcoeff{1},currentcoeff{2},audio) + eqlzd;
             end            
         end

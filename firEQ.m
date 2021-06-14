e695cc5bf120334d23daaad7c88e646236286c84
Ahmed Osama Elshafei <79366909+AhmedOsamaElShafei@users.noreply.github.com>
2021-06-14 23:30:33 +0200

    Adjusting Orders

--- firEQ.m
+++ firEQ.m
@@ -3,22 +3,22 @@ classdef firEQ
         coeffecients = {}
         bands = {170,[170 310],[310 600],[600 1000],[1000 3000],[3000 6000],[6000 12000],[12000 14000],[14000 16000]}
         gains
-        orders = [8,4,5,5,8,6,8,7,8]
+        orders = [1,1,1,1,1,1,1,1,1]
         EqlzdAudio
     end
     methods
         function obj = firEQ(gains,audio,fs)
             obj.gains = gains;
-            fs = fs/2;
+            fn = fs/2;
             for i = 1:9
-                obj.coeffecients = fir1(obj.orders(i),obj.bands{i} / fs);
+                obj.coeffecients{i} = fir1(obj.orders(i),obj.bands{i} / fn);
             end
             obj.EqlzdAudio = obj.eqaudio(audio);
         end
         function eqlzd = eqaudio(obj,audio)
             eqlzd = zeros(size(audio,1),size(audio,2));
             for i = 1:9
-                currentcoeff = obj.coeffecients;
+                currentcoeff = obj.coeffecients{i};
                 eqlzd = filter(obj.gains(i)*currentcoeff,currentcoeff,audio) + eqlzd;
             end            
         end

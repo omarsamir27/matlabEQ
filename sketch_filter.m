291b268ec63944274c13c28d09a11a05305fffb2
Omar Mohamed Samir <omarmohameddrive@gmail.com>
2021-06-14 22:14:35 +0200

    removed sample audio

--- sketch_filter.m
+++ sketch_filter.m
@@ -1,24 +1,25 @@
-function sketch_filter(b,a)
-fig_freq=figure
-freqz(b,a)
-grid on
-fig_resp=figure
-[stp,t1] = stepz(b,a);
-[imp,t2] = impz(b,a);
-subplot(2,1,1)
-plot(imp,t2)
-grid on
-ylabel('impulse response');
-xlabel('time');
-subplot(2,1,2)
-plot(stp,t1)
-grid on
-ylabel('step response');
-xlabel('time');
-fig_pz=figure
-zplane(b,a)
-grid on
-saveas(fig_freq,'frequency_response.png');
-saveas(fig_resp,'imp_step_response.png');
-saveas(fig_pz,'pole_zero.png');
+function sketch_filter(b,a,bandNum)
+    Band = concat('Band',num2str(bandNum));
+    fig_freq=figure
+    freqz(b,a)
+    grid on
+    fig_resp=figure
+    [stp,t1] = stepz(b,a);
+    [imp,t2] = impz(b,a);
+    subplot(2,1,1)
+    plot(imp,t2)
+    grid on
+    ylabel('impulse response');
+    xlabel('time');
+    subplot(2,1,2)
+    plot(stp,t1)
+    grid on
+    ylabel('step response');
+    xlabel('time');
+    fig_pz=figure
+    zplane(b,a)
+    grid on
+    saveas(fig_freq,concat(Band,'frequency_response.png'));
+    saveas(fig_resp,concat(Band,'imp_step_response.png'));
+    saveas(fig_pz,concat(Band,'pole_zero.png'));
 end
